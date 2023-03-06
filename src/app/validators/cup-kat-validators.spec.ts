141e89ab7e6ae9001550d77da824b538cb319a67
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2023-03-06 12:35:19 -0300

    componentes

---
+++ src/app/validators/cup-kat-validators.spec.ts
@@ -0,0 +1,7 @@
+import { CupKatValidators } from './cup-kat-validators';
+
+describe('CupKatValidators', () => {
+  it('should create an instance', () => {
+    expect(new CupKatValidators()).toBeTruthy();
+  });
+});
