05ae317f6a3da925024f07eabbd9c7f578f3c31a
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2023-04-03 20:52:44 -0300

    ícones de categoria

--- src/app/common/product-category.ts
+++ src/app/common/product-category.ts
@@ -1,7 +1,7 @@
 export class ProductCategory {
     
     constructor(public id: number,
-                public categoryName: string) {
+                public categoryName: string, public imageUrl: string) {
     
     }
 }
