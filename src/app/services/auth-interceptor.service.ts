85506bbf9593b0b90e8002a3f4cfc4ed34a73220
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2023-06-11 01:03:00 -0300

    correção endpoint

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -18,7 +18,7 @@ export class AuthInterceptorService implements HttpInterceptor {
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
     
     // Only add an access token for secured endpoints
-    const securedEndpoints = ['http://localhost:8080/api/orders'];
+    const securedEndpoints = ['http://apispringboot-env.eba-gqfpymtm.us-east-2.elasticbeanstalk.com/api/orders'];
 
     if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
       
