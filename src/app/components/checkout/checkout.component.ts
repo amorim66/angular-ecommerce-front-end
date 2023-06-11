9f678ea55f4c107366fa466ad04d632ba2009c77
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2023-06-10 23:09:39 -0300

    order tracking user

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -32,6 +32,7 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage: Storage = sessionStorage;
 
   constructor(private formBuilder: FormBuilder,
     private shopFormService: ShopFormServiceService,
@@ -44,6 +45,10 @@ export class CheckoutComponent implements OnInit {
 
     this.reviewCartDetails();
 
+    // read the user's email address from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail') || '{}');
+
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('',
@@ -55,7 +60,7 @@ export class CheckoutComponent implements OnInit {
           [Validators.required,
            Validators.minLength(2),
            CupKatValidators.notOnlyWhitespace]),
-        email: new FormControl('',
+        email: new FormControl(theEmail,
           [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9._]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
